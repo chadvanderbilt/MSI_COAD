fdf8825a59f34c9ce4492acb6a7c53f67d94b3c4
chadvanderbilt <34893722+chadvanderbilt@users.noreply.github.com>
2023-08-23 23:13:31 -0600

    Add files via upload

---
+++ awk.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+path=$1
+
+directory=$(dirname "$path")
+
+echo $directory
+
+rm $directory/awk_out_final_full.txt
+rm $directory/awk_tmp2.txt
+
+awk '{print $0, FILENAME}' \
+   $path/*.tax >> \
+   $directory/awk_tmp2.txt
+
+while IFS= read -r row; do
+   read var1 var2 var3 var5 < <(echo $row);
+   file=$(basename "$var5")
+   file_rm="${file%.fasta.bl.tax}"
+   echo $file_rm
+   echo $var1 $var2 $file_rm >> $directory/awk_out_final_full.txt
+done < $directory/awk_tmp2.txt
+
