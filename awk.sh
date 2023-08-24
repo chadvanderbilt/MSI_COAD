f73adb34598dfaeed3b376747bd76fd8a7b443a5
chadvanderbilt <34893722+chadvanderbilt@users.noreply.github.com>
2023-08-23 23:30:12 -0600

    Update awk.sh

--- awk.sh
+++ awk.sh
@@ -7,10 +7,13 @@ echo $directory
 
 rm $directory/awk_out_final_full.txt
 rm $directory/awk_tmp2.txt
+rm $directory/awk_tmp3.txt
 
 awk '{print $0, FILENAME}' \
    $path/*.tax >> \
    $directory/awk_tmp2.txt
+
+cat $directory/awk_tmp2.txt | grep -v "taxID" > $directory/awk_tmp3.txt
 
 while IFS= read -r row; do
    read var1 var2 var3 var5 < <(echo $row);
@@ -18,5 +21,4 @@ while IFS= read -r row; do
    file_rm="${file%.fasta.bl.tax}"
    echo $file_rm
    echo $var1 $var2 $file_rm >> $directory/awk_out_final_full.txt
-done < $directory/awk_tmp2.txt
-
+done < $directory/awk_tmp3.txt
